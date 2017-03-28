795e5512dcc846c7fffc9a22867d8b7756b73fe3
pritam-das <pritam.das@mail.utoronto.ca>
2017-03-28 09:48:55 -0400

    example 1 pseudocode done

---
+++ pseudocode1.rb
@@ -0,0 +1,19 @@
+number = range from 1 to 100
+
+loop the following code starting number=0 until number= 100 :
+{
+  if number%2 ==0
+    {
+      print "#{number} is even"
+
+    }
+
+  else
+    {
+      print "#{number} is odd"
+
+    }
+
+  increment number by 1  
+
+}
